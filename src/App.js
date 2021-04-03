c8be6693b5358a14f6bea99554451b1a2fec2a6e
Charles May <charlie@Charless-MacBook-Pro.local>
2021-04-03 10:55:35 -0500

    data fetched succesfullly

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect }from "react";
+import "./App.css";
+import axios from 'axios';
+import Character from "./components/Character";
+
+const URL = 'https://swapi.dev/api/people/'
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  //state management here - going to set character data from after I fetch the info from the api and then pass the data through props to the character component
+  const [characterData, setCharacterData] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  useEffect(() => {
+    const fetchCharacter = () =>
+      axios
+        .get(`${URL}`)
+        .then((res) => {
+          setCharacterData(res.data);
+        })
+        .catch((err) => {
+          debugger;
+        });
+    fetchCharacter();
+  },[]);
+
+  characterData.forEach(obj => {
+    console.log(obj.name)
+  })
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characterData={characterData}/>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacterDiv = styled.div`
+    border: solid red 1px;
+    margin: 5% 20%;
+
+`;
+
+const StyledCharacter = styled.div`
+    border: solid black 1px;
+    margin: 2% 2%;
+    display: flex;
+    justify-content: space-between; 
+`;
+
+
+
+export default function Character({ characterData }) {
+  return (
+    <StyledCharacterDiv>
+      <StyledCharacter>
+        <h2>Insert Name</h2>
+        <h2>3/6/86</h2>
+      </StyledCharacter>
+    </StyledCharacterDiv>
+  );
+}
